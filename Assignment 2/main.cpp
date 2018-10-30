96f0264683467a94b8f9fde5ecbb000a5c98dce9
Chris Griffis <chrisiffis@gmail.com>
2018-10-30 04:42:14 -0700

    Initial Commit

---
+++ Assignment 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 2
+//
+//  Created by Chris Griffis on 10/30/18.
+//  Copyright © 2018 Chris Griffis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
